bab04898cdaad3b4863283a3be5e1ae69606ca54
jeongjoonsup <penny4860@gmail.com>
2019-11-30 16:58:28 +0900

    model 관련 부분을 별도의 모듈로 extract

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 
 import 'babel-polyfill';
-import * as tf from '@tensorflow/tfjs';
-tf.ENV.set('WEBGL_PACK', false);  // This needs to be done otherwise things run very slow v1.0.4
 
 // ImageElement class
 // UI : stylize button 관련된 동작을 정의
 // Model : tensorflow model 관련된 종작을 정의
 
-
+import StyleModel from './stylemodel';
 
 // 1. html 에 의존성이 있는 element 들을 정의
 class Element {
@@ -71,46 +69,6 @@ class Element {
         }
       }
 }
-
-
-class StyleModel {
-    constructor () {
-        // this.styleNet
-        // this.transformNet
-    }
-    async loadMobileNetStyleModel() {
-        if (!this.styleNet) {
-          this.styleNet = await tf.loadGraphModel(
-            'saved_model_style_js/model.json');
-        }
-    }
-    
-    async loadSeparableTransformerModel() {
-        if (!this.transformNet) {
-            this.transformNet = await tf.loadGraphModel(
-            'saved_model_transformer_separable_js/model.json'
-            );
-        }
-    }
-
-    async run(contentImg, styleImg, stylizedImg) {
-        await tf.nextFrame();
-        // this.styleButton.textContent = 'Generating 100D style representation';
-        await tf.nextFrame();
-        let bottleneck = await tf.tidy(() => {
-          return this.styleNet.predict(tf.browser.fromPixels(styleImg).toFloat().div(tf.scalar(255)).expandDims());
-        })
-        // this.styleButton.textContent = 'Stylizing image...';
-        await tf.nextFrame();
-        const stylized = await tf.tidy(() => {
-          return this.transformNet.predict([tf.browser.fromPixels(contentImg).toFloat().div(tf.scalar(255)).expandDims(), bottleneck]).squeeze();
-        })
-        await tf.browser.toPixels(stylized, stylizedImg);
-        bottleneck.dispose();  // Might wanna keep this around
-        stylized.dispose();
-    }
-}
-
 
 /**
  * Main application to start on window load
